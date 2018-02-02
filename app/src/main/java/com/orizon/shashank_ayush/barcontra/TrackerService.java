37d7fa8662c6a069529a1c3fba3b18d69be0459e
Shashank Gupta <gshashank84@gmail.com>
2018-02-02 19:24:39 +0530

    barcontra

---
+++ app/src/main/java/com/orizon/shashank_ayush/barcontra/TrackerService.java
@@ -0,0 +1,60 @@
+package com.orizon.shashank_ayush.barcontra;
+
+        import android.app.PendingIntent;
+        import android.app.Service;
+        import android.content.BroadcastReceiver;
+        import android.content.Context;
+        import android.content.Intent;
+        import android.content.IntentFilter;
+        import android.os.IBinder;
+        import android.support.v4.app.NotificationCompat;
+        import android.util.Log;
+
+public class TrackerService extends Service {
+
+    private static final String TAG = TrackerService.class.getSimpleName();
+
+    @Override
+    public IBinder onBind(Intent intent) {return null;}
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        buildNotification();
+        loginToFirebase();
+    }
+
+    private void buildNotification() {
+        String stop = "stop";
+        registerReceiver(stopReceiver, new IntentFilter(stop));
+        PendingIntent broadcastIntent = PendingIntent.getBroadcast(
+                this, 0, new Intent(stop), PendingIntent.FLAG_UPDATE_CURRENT);
+        // Create the persistent notification
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this)
+                .setContentTitle(getString(R.string.app_name))
+                .setContentText(getString(R.string.notification_text))
+                .setOngoing(true)
+                .setContentIntent(broadcastIntent)
+                .setSmallIcon(R.drawable.ic_tracker);
+        startForeground(1, builder.build());
+    }
+
+    protected BroadcastReceiver stopReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Log.d(TAG, "received stop broadcast");
+            // Stop the service when the notification is tapped
+            unregisterReceiver(stopReceiver);
+            stopSelf();
+        }
+    };
+
+    private void loginToFirebase() {
+        // Functionality coming next step
+    }
+
+    private void requestLocationUpdates() {
+        // Functionality coming next step
+    }
+
+}

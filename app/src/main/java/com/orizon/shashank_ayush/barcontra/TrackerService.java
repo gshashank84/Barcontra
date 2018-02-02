47624dd9871ba5c27ef743ab07a788914160472f
Shashank Gupta <gshashank84@gmail.com>
2018-02-02 20:11:52 +0530

    barcontra

--- app/src/main/java/com/orizon/shashank_ayush/barcontra/TrackerService.java
+++ app/src/main/java/com/orizon/shashank_ayush/barcontra/TrackerService.java
@@ -6,9 +6,24 @@ package com.orizon.shashank_ayush.barcontra;
         import android.content.Context;
         import android.content.Intent;
         import android.content.IntentFilter;
+        import android.content.pm.PackageManager;
+        import android.location.Location;
         import android.os.IBinder;
         import android.support.v4.app.NotificationCompat;
+        import android.support.v4.content.ContextCompat;
         import android.util.Log;
+
+        import com.google.android.gms.location.FusedLocationProviderClient;
+        import com.google.android.gms.location.LocationCallback;
+        import com.google.android.gms.location.LocationRequest;
+        import com.google.android.gms.location.LocationResult;
+        import com.google.android.gms.location.LocationServices;
+        import com.google.android.gms.tasks.OnCompleteListener;
+        import com.google.android.gms.tasks.Task;
+        import com.google.firebase.auth.AuthResult;
+        import com.google.firebase.auth.FirebaseAuth;
+        import com.google.firebase.database.DatabaseReference;
+        import com.google.firebase.database.FirebaseDatabase;
 
 public class TrackerService extends Service {
 
@@ -50,11 +65,47 @@ public class TrackerService extends Service {
     };
 
     private void loginToFirebase() {
-        // Functionality coming next step
+        // Authenticate with Firebase, and request location updates
+        String email = getString(R.string.firebase_email);
+        String password = getString(R.string.firebase_password);
+        FirebaseAuth.getInstance().signInWithEmailAndPassword(
+                email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>(){
+            @Override
+            public void onComplete(Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    Log.d(TAG, "firebase auth success");
+                    requestLocationUpdates();
+                } else {
+                    Log.d(TAG, "firebase auth failed");
+                }
+            }
+        });
     }
 
     private void requestLocationUpdates() {
-        // Functionality coming next step
+        LocationRequest request = new LocationRequest();
+        request.setInterval(10000);
+        request.setFastestInterval(5000);
+        request.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        FusedLocationProviderClient client = LocationServices.getFusedLocationProviderClient(this);
+        final String path = getString(R.string.firebase_path) + "/" + getString(R.string.transport_id);
+        int permission = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION);
+        if (permission == PackageManager.PERMISSION_GRANTED) {
+            // Request location updates and when an update is
+            // received, store the location in Firebase
+            client.requestLocationUpdates(request, new LocationCallback() {
+                @Override
+                public void onLocationResult(LocationResult locationResult) {
+                    DatabaseReference ref = FirebaseDatabase.getInstance().getReference(path);
+                    Location location = locationResult.getLastLocation();
+                    if (location != null) {
+                        Log.d(TAG, "location update " + location);
+                        ref.setValue(location);
+                    }
+                }
+            }, null);
+        }
     }
 
 }

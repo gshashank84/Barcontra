aab3aad60799fe1d94cf83b46f73212f6076ca30
Shashank Gupta <gshashank84@gmail.com>
2018-02-03 15:53:51 +0530

    barcontra

--- app/src/main/java/com/orizon/shashank_ayush/barcontra/StartActivity.java
+++ app/src/main/java/com/orizon/shashank_ayush/barcontra/StartActivity.java
@@ -1,7 +1,11 @@
 package com.orizon.shashank_ayush.barcontra;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class StartActivity extends AppCompatActivity {
 
@@ -9,5 +13,25 @@ public class StartActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+        Button btn1 = (Button)findViewById(R.id.button3);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(new Intent(StartActivity.this, MainActivity.class));
+            }
+        });
+
+        Button btn2 = (Button)findViewById(R.id.button2);
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(StartActivity.this, MapsActivity.class));
+            }
+        });
     }
+
+
+
+
 }
